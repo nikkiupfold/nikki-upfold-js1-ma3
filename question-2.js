9cf0b23e2cf8a7d892d0859ba0878afe909611c1
Nikki <nikkiupfold@gmail.com>
2022-02-13 23:58:15 +0100

    done

--- question-2.js
+++ question-2.js
@@ -1,37 +1,34 @@
-const loading = document.querySelector(".loading");
-const resultsContainer = document.querySelector(".results");
-const url =
-  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-200625380b134a1ba83ed6f88570b692";
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=200625380b134a1ba83ed6f88570b692";
+
+const descriptionsContainer = document.querySelector(".descriptions");
 
 async function getGames() {
-  try {
-    const response = await fetch(url);
 
-    const results = await response.json();
+    try {
+        const response = await fetch(url);
 
-    const facts = results.results;
+        const results = await response.json();
 
-    resultsContainer.innerHTML = "";
+        const descriptions = results.results;
 
-    for (i = 0; i < facts.length; i++) {
-      if (i === 8) {
-        break;
-      }
+        descriptionsContainer.innerHTML = "";
 
-      resultsContainer.innerHTML += `<div class="cards">
-                                                <h3>Name</h3>
-                                                <p>${facts[i].name}</p>
-                                                <h3>Rating</h3>
-                                                <p>${facts[i].rating}</p>
-                                                <h3>Tags</h3>
-                                                <p>${facts[i].tags.length}</p> 
-                                            </div>`;
+        for (let i = 0; i < descriptions.length; i++) {
+
+            if (i === 8) {
+                break;
+            }
+
+            let name = descriptions[i].name;
+            let rating = descriptions[i].rating;
+            let amountOfTags = descriptions[i].tags.length;
+            let convertedAmountOfTags = amountOfTags.toString();
+
+            descriptionsContainer.innerHTML += `<div class="description"><p>Name: ${name}</p> <p>Rating: ${rating}</p> <p> Amount of tags: ${convertedAmountOfTags}</p></div>`;
+        }
+    } catch (error) {
+        descriptionsContainer.innerHTML = errorMessage("error", "There was an error when fetching API.");
     }
-  } catch (error) {
-    resultsContainer.innerHTML = displayError(
-      "An error occured when calling for API"
-    );
-  }
 }
 
 getGames();
